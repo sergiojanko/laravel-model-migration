8c577e0c4807ac3dbb91beff353bed971afadc63
sergiojanko <janko.sergio1998@gmail.com>
2021-10-08 16:37:21 +0200

    migrations

---
+++ database/migrations/2021_10_08_141803_update_travels_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('travels', function (Blueprint $table) {
+            $table->text("description");
+            $table->tinyInteger("duration");
+            $table->foreignId("id_transport");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            $table->dropColumn("description");
+            $table->dropColumn("duration");
+            $table->dropColumn("id_transport");
+        });
+    }
+}
--- database/migrations/2021_10_08_142747_create_transports_table.php
+++ database/migrations/2021_10_08_142747_create_transports_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTravelsTable extends Migration
+class CreateTransportsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateTravelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('travels', function (Blueprint $table) {
+        Schema::create('transports', function (Blueprint $table) {
             $table->id();
-            $table->string('destination', 100);
-            $table->float('price', 8, 2);
+            $table->string('description', 100);
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreateTravelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('travels');
+        Schema::dropIfExists('transports');
     }
 }
